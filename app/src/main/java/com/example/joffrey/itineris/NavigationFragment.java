68066625e4497afca434f8cbd16aa28b198872f7
JoffreyDuc <joffreyduc@gmail.com>
2018-09-20 12:35:31 +0200

    Initial commit

---
+++ app/src/main/java/com/example/joffrey/itineris/NavigationFragment.java
@@ -0,0 +1,25 @@
+package com.example.joffrey.itineris;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class NavigationFragment extends Fragment {
+    public static NavigationFragment newInstance() {
+        NavigationFragment fragment = new NavigationFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_navigation, container, false);
+    }
+}
