d2e4a1be4231fd6b61db10008bb72dc10e5520da
JoffreyDuc <joffreyduc@gmail.com>
2018-09-25 15:57:22 +0200

    Initial commit

---
+++ app/src/main/java/com/example/joffrey/itineris/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.joffrey.itineris;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ViewPager viewPager;
+    private BottomBarAdapter pagerAdapter;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment selectedFragment = null;
+            switch (item.getItemId()) {
+                case R.id.navigation_map:
+                    selectedFragment = MapFragment.newInstance();
+                    break;
+                case R.id.navigation_camera:
+                    selectedFragment = NavigationFragment.newInstance();
+                    break;
+                case R.id.navigation_settings:
+                    selectedFragment = SettingsFragment.newInstance();
+                    break;
+            }
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+            transaction.replace(R.id.frame_layout, selectedFragment);
+            transaction.commit();
+            return true;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.navigation);
+
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame_layout, MapFragment.newInstance());
+        transaction.commit();
+
+//
+//        viewPager = (ViewPager) findViewById(R.id.pager);
+//        pagerAdapter = new BottomBarAdapter(getSupportFragmentManager());
+//
+//        viewPager.setAdapter(pagerAdapter);
+//
+//        pagerAdapter.addFragments(new MapFragment());
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
