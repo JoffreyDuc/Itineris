2a81c257a0d5a8bb2cb89ee576cb53d4361081b7
Alexandre-Moreau <alexandre.moreau2@etu.univ-savoie.fr>
2018-10-03 11:49:15 +0200

    Début points

--- app/src/main/java/com/example/joffrey/itineris/NavigationFragment.java
+++ app/src/main/java/com/example/joffrey/itineris/NavigationFragment.java
@@ -18,8 +18,7 @@ public class NavigationFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_navigation, container, false);
     }
 }
