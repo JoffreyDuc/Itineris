a371683438383ea3942e92aea88f945723f09f14
JoffreyDuc <joffreyduc@gmail.com>
2018-10-03 11:56:18 +0200

    Fin séance

--- app/src/main/java/com/example/joffrey/itineris/NavigationFragment.java
+++ app/src/main/java/com/example/joffrey/itineris/NavigationFragment.java
@@ -18,8 +18,11 @@ public class NavigationFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_navigation, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_navigation, container, false);
+
+        // Hmm
+
+        return rootView;
     }
 }
