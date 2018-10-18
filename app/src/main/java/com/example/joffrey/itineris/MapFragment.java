fb308705df1ba32991da39c747d5080ef878b816
JoffreyDuc <joffreyduc@gmail.com>
2018-10-18 20:41:40 +0200

    Localisation fonctionnelle

--- app/src/main/java/com/example/joffrey/itineris/MapFragment.java
+++ app/src/main/java/com/example/joffrey/itineris/MapFragment.java
@@ -1,23 +1,60 @@
 package com.example.joffrey.itineris;
 
 import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
-import android.graphics.Color;
 import android.graphics.Paint;
+import android.location.Location;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.Toast;
 
-public class MapFragment extends Fragment {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+
+import java.util.ArrayList;
+
+// Inspired by https://medium.com/@ssaurel/getting-gps-location-on-android-with-fused-location-provider-api-1001eb549089
+
+public class MapFragment extends Fragment implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
     private ImageView iv_canvas;
     private Canvas canvas;
     private Paint paint;
+    double latitudeMapTop = 45.643159;
+    double latitudeMapBottom = 45.637620;
+    double longitudeMapLeft = 5.862951;
+    double longitudeMapRight = 5.875756;
+    double height = 500.0;
+    double width = 500.0;
+
+    private Location location;
+    private GoogleApiClient googleApiClient;
+    private static final int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
+    private LocationRequest locationRequest;
+    private static final long UPDATE_INTERVAL = 5000, FASTEST_INTERVAL = 5000; // = 5 seconds
+    // lists for permissions
+    private ArrayList<String> permissionsToRequest;
+    private ArrayList<String> permissionsRejected = new ArrayList<>();
+    private ArrayList<String> permissions = new ArrayList<>();
+    // integer for permissions results request
+    private static final int ALL_PERMISSIONS_RESULT = 1011;
+
 
     public static MapFragment newInstance() {
         MapFragment fragment = new MapFragment();
@@ -35,14 +72,217 @@ public class MapFragment extends Fragment {
 
         ActivityCompat.requestPermissions(getActivity(),new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
 
-        View v = new PositionCanvas(getActivity().getApplicationContext());
+        View v = new PositionCanvas(getActivity().getApplicationContext(), 250 ,250);
         Bitmap bitmap = Bitmap.createBitmap(500, 500, Bitmap.Config.ARGB_8888); //width, height,..
         Canvas canvas = new Canvas(bitmap);
         v.draw(canvas);
+
         ImageView iv = (ImageView) rootView.findViewById(R.id.ivCanvas);
         iv.setImageBitmap(bitmap);
+
+        // we add permissions we need to request location of the users
+        permissions.add(Manifest.permission.ACCESS_FINE_LOCATION);
+        permissions.add(Manifest.permission.ACCESS_COARSE_LOCATION);
+
+        permissionsToRequest = permissionsToRequest(permissions);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (permissionsToRequest.size() > 0) {
+                requestPermissions(permissionsToRequest.toArray(
+                        new String[permissionsToRequest.size()]), ALL_PERMISSIONS_RESULT);
+            }
+        }
+
+        // we build google api client
+        googleApiClient = new GoogleApiClient.Builder(getContext()).
+                addApi(LocationServices.API).
+                addConnectionCallbacks(this).
+                addOnConnectionFailedListener(this).build();
 
         return rootView;
     }
 
+    private ArrayList<String> permissionsToRequest(ArrayList<String> wantedPermissions) {
+        ArrayList<String> result = new ArrayList<>();
+
+        for (String perm : wantedPermissions) {
+            if (!hasPermission(perm)) {
+                result.add(perm);
+            }
+        }
+
+        return result;
+    }
+
+    private boolean hasPermission(String permission) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            return getActivity().checkSelfPermission(permission) == PackageManager.PERMISSION_GRANTED;
+        }
+
+        return true;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        if (googleApiClient != null) {
+            googleApiClient.connect();
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        if (!checkPlayServices()) {
+            Log.d("d", "LOCATION IS : " + location.getLatitude() + ", " + location.getLongitude());
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        // stop location updates
+        if (googleApiClient != null  &&  googleApiClient.isConnected()) {
+            LocationServices.FusedLocationApi.removeLocationUpdates(googleApiClient, this);
+            googleApiClient.disconnect();
+        }
+    }
+
+    private boolean checkPlayServices() {
+        GoogleApiAvailability apiAvailability = GoogleApiAvailability.getInstance();
+        int resultCode = apiAvailability.isGooglePlayServicesAvailable(getContext());
+
+        if (resultCode != ConnectionResult.SUCCESS) {
+            if (apiAvailability.isUserResolvableError(resultCode)) {
+                apiAvailability.getErrorDialog(getActivity(), resultCode, PLAY_SERVICES_RESOLUTION_REQUEST);
+            } else {
+                getActivity().finish();
+            }
+
+            return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        if (ActivityCompat.checkSelfPermission(getContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                &&  ActivityCompat.checkSelfPermission(getContext(),
+                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+
+        // Permissions ok, we get last location
+        location = LocationServices.FusedLocationApi.getLastLocation(googleApiClient);
+
+        if (location != null) {
+            Log.d("d", "LOCATION IS : " + location.getLatitude() + ", " + location.getLongitude());
+        }
+
+        startLocationUpdates();
+    }
+
+    private void startLocationUpdates() {
+        locationRequest = new LocationRequest();
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locationRequest.setInterval(UPDATE_INTERVAL);
+        locationRequest.setFastestInterval(FASTEST_INTERVAL);
+
+        if (ActivityCompat.checkSelfPermission(getContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                &&  ActivityCompat.checkSelfPermission(getContext(),
+                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(getContext(), "You need to enable permissions to display location !", Toast.LENGTH_SHORT).show();
+        }
+
+        LocationServices.FusedLocationApi.requestLocationUpdates(googleApiClient, locationRequest, this);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        if (location != null) {
+            Log.d("d", "LOCATION CHANGED TO : " + location.getLatitude() + ", " + location.getLongitude());
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch(requestCode) {
+            case ALL_PERMISSIONS_RESULT:
+                for (String perm : permissionsToRequest) {
+                    if (!hasPermission(perm)) {
+                        permissionsRejected.add(perm);
+                    }
+                }
+
+                if (permissionsRejected.size() > 0) {
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                        if (shouldShowRequestPermissionRationale(permissionsRejected.get(0))) {
+                            new AlertDialog.Builder(getContext()).
+                                    setMessage("Ces autorisations sont nécessaires pour l'utilisation de votre position. Merci de les autoriser.").
+                                    setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialogInterface, int i) {
+                                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                                                requestPermissions(permissionsRejected.
+                                                        toArray(new String[permissionsRejected.size()]), ALL_PERMISSIONS_RESULT);
+                                            }
+                                        }
+                                    }).setNegativeButton("Cancel", null).create().show();
+
+                            return;
+                        }
+                    }
+                } else {
+                    if (googleApiClient != null) {
+                        googleApiClient.connect();
+                    }
+                }
+
+                break;
+        }
+    }
 }
+
+/* pour dessiner :
+    Point2D p = gpsToMap(getCoordinates());
+
+    canvas.drawCircle((float) p.getX(), (float) ((float) 500.0 - p.getY()), 7, paint);
+
+méthodes utiles :
+    private Point2D gpsToMap(Point2D gpsPoint) {
+
+        double x = (gpsPoint.getX() - this.longitudeMapLeft) / (this.longitudeMapRight - this.longitudeMapLeft) * this.height;
+        double y = (gpsPoint.getY() - this.latitudeMapBottom) / (this.latitudeMapTop - this.latitudeMapBottom) * this.width;
+
+        return new Point2D(x, y);
+    }
+
+    private Point2D getCoordinates() {
+        Location location = getLocation();
+        Log.d("D", location.getLatitude() + ", " + location.getLongitude());
+        return new Point2D(location.getLongitude(), location.getLatitude());
+        //return new Point2D(this.longitudeMapLeft, this.latitudeMapBottom);
+    }
+
+    public boolean checkLocationPermission(){
+        String permission = "android.permission.ACCESS_FINE_LOCATION";
+        int res = this.context.checkCallingOrSelfPermission(permission);
+        return (res == PackageManager.PERMISSION_GRANTED);
+    }
+ */
--- app/src/main/java/com/example/joffrey/itineris/PositionCanvas.java
+++ app/src/main/java/com/example/joffrey/itineris/PositionCanvas.java
@@ -4,45 +4,26 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.location.Location;
-import android.location.LocationListener;
-import android.os.Bundle;
 import android.view.View;
 
-import java.util.Timer;
-
-public class PositionCanvas extends View implements LocationListener {
+public class PositionCanvas extends View {
     Canvas canvas;
+    float x, y;
 
-    public PositionCanvas(Context context) {
+    public PositionCanvas(Context context, float x, float y) {
         super(context);
+        this.x = x;
+        this.y = y;
     }
 
     @Override
     protected void onDraw(Canvas canvas){
         super.onDraw(canvas);
         this.canvas = canvas;
+        // Dessiner le point de la position actuelle
+        Paint paint = new Paint();
+        paint.setColor(Color.RED);
+        canvas.drawCircle(x, (float) 500.0 - y, 10, paint);
     }
 
-    @Override
-    public void onLocationChanged(Location location) {
-        // Redessiner le canvas (à voir)
-        // Dessiner le point de la posision actuelle
-        new PointDrawer(canvas, getContext(), new Point2D(location.getLongitude(), location.getLatitude()));
-    }
-
-    @Override
-    public void onStatusChanged(String s, int i, Bundle bundle) {
-
-    }
-
-    @Override
-    public void onProviderEnabled(String s) {
-
-    }
-
-    @Override
-    public void onProviderDisabled(String s) {
-        // Redessiner le canvas (pour enlever les points)
-    }
 }
